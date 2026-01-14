3bf44ff558450d2c0f4757dcf4ebbca813595b15
Muckenbatscher <christi.braeuner97@gmail.com>
2026-01-14 17:31:55 +0100

    added all color role properties to the generation of the Csharp test class

--- java/CsharpTestThemeCreator.java
+++ java/CsharpTestThemeCreator.java
@@ -14,11 +14,12 @@ public class CsharpTestThemeCreator {
     public static int GetColorCount(){
         return 8;
     }
+
     public static CsharpTestThemeClass GetCsharpTestThemeClass(int colorIndex,
-                                                 boolean isDark,
-                                                 ContrastLevelType contrastLevelType,
-                                                 ColorSpec.SpecVersion specVersion,
-                                                 String variantName){
+                                                               boolean isDark,
+                                                               ContrastLevelType contrastLevelType,
+                                                               ColorSpec.SpecVersion specVersion,
+                                                               String variantName){
         var hct = GetColor(colorIndex);
         var colorName = GetColorName(colorIndex);
         var contrastLevel = GetContrastLevelValue(contrastLevelType);
@@ -30,6 +31,7 @@ public class CsharpTestThemeCreator {
                 + GetContrastLevelName(contrastLevelType) + "_"
                 + GetSpecName(specVersion) + "_"
                 + colorName;
+
         var builder = new StringBuilder();
         builder.append("using MaterialTheming.MaterialDesign;").append("\r\n");
         builder.append("\r\n");
@@ -46,35 +48,95 @@ public class CsharpTestThemeCreator {
                 ? "Spec2021" : "Spec2025";
         builder.append("    ").append(GetCsharpProperty("SpecVersion", "SpecVersion", "SpecVersion." + specVersionCsharp)).append("\r\n");
         builder.append("\r\n");
+
+        // Primary
+        builder.append("    ").append("// Primary").append("\r\n");
         builder.append("    ").append(GetCsharpColorProperty("Primary", scheme.getPrimary())).append("\r\n");
         builder.append("    ").append(GetCsharpColorProperty("OnPrimary", scheme.getOnPrimary())).append("\r\n");
         builder.append("    ").append(GetCsharpColorProperty("PrimaryContainer", scheme.getPrimaryContainer())).append("\r\n");
         builder.append("    ").append(GetCsharpColorProperty("OnPrimaryContainer", scheme.getOnPrimaryContainer())).append("\r\n");
-        builder.append("\r\n");
+
+        // Secondary
+        builder.append("    ").append("// Secondary").append("\r\n");
         builder.append("    ").append(GetCsharpColorProperty("Secondary", scheme.getSecondary())).append("\r\n");
         builder.append("    ").append(GetCsharpColorProperty("OnSecondary", scheme.getOnSecondary())).append("\r\n");
         builder.append("    ").append(GetCsharpColorProperty("SecondaryContainer", scheme.getSecondaryContainer())).append("\r\n");
         builder.append("    ").append(GetCsharpColorProperty("OnSecondaryContainer", scheme.getOnSecondaryContainer())).append("\r\n");
-        builder.append("\r\n");
+
+        // Tertiary
+        builder.append("    ").append("// Tertiary").append("\r\n");
         builder.append("    ").append(GetCsharpColorProperty("Tertiary", scheme.getTertiary())).append("\r\n");
         builder.append("    ").append(GetCsharpColorProperty("OnTertiary", scheme.getOnTertiary())).append("\r\n");
         builder.append("    ").append(GetCsharpColorProperty("TertiaryContainer", scheme.getTertiaryContainer())).append("\r\n");
         builder.append("    ").append(GetCsharpColorProperty("OnTertiaryContainer", scheme.getOnTertiaryContainer())).append("\r\n");
-        builder.append("\r\n");
+
+        // Error
+        builder.append("    ").append("// Error").append("\r\n");
         builder.append("    ").append(GetCsharpColorProperty("Error", scheme.getError())).append("\r\n");
         builder.append("    ").append(GetCsharpColorProperty("OnError", scheme.getOnError())).append("\r\n");
         builder.append("    ").append(GetCsharpColorProperty("ErrorContainer", scheme.getErrorContainer())).append("\r\n");
         builder.append("    ").append(GetCsharpColorProperty("OnErrorContainer", scheme.getOnErrorContainer())).append("\r\n");
-        builder.append("\r\n");
+
+        // Surface
+        builder.append("    ").append("// Surface").append("\r\n");
         builder.append("    ").append(GetCsharpColorProperty("Surface", scheme.getSurface())).append("\r\n");
+        builder.append("    ").append(GetCsharpColorProperty("SurfaceVariant", scheme.getSurfaceVariant())).append("\r\n");
         builder.append("    ").append(GetCsharpColorProperty("OnSurface", scheme.getOnSurface())).append("\r\n");
         builder.append("    ").append(GetCsharpColorProperty("OnSurfaceVariant", scheme.getOnSurfaceVariant())).append("\r\n");
-        builder.append("\r\n");
+        builder.append("    ").append(GetCsharpColorProperty("SurfaceDim", scheme.getSurfaceDim())).append("\r\n");
+        builder.append("    ").append(GetCsharpColorProperty("SurfaceBright", scheme.getSurfaceBright())).append("\r\n");
+        builder.append("    ").append(GetCsharpColorProperty("SurfaceTint", scheme.getSurfaceTint())).append("\r\n");
+
+        // Background
+        builder.append("    ").append("// Background").append("\r\n");
+        builder.append("    ").append(GetCsharpColorProperty("Background", scheme.getBackground())).append("\r\n");
+        builder.append("    ").append(GetCsharpColorProperty("OnBackground", scheme.getOnBackground())).append("\r\n");
+
+        // Outline
+        builder.append("    ").append("// Outline").append("\r\n");
+        builder.append("    ").append(GetCsharpColorProperty("Outline", scheme.getOutline())).append("\r\n");
+        builder.append("    ").append(GetCsharpColorProperty("OutlineVariant", scheme.getOutlineVariant())).append("\r\n");
+
+        // Shadow
+        builder.append("    ").append("// Shadow").append("\r\n");
+        builder.append("    ").append(GetCsharpColorProperty("Shadow", scheme.getShadow())).append("\r\n");
+        builder.append("    ").append(GetCsharpColorProperty("Scrim", scheme.getScrim())).append("\r\n");
+
+        // Inverse
+        builder.append("    ").append("// Inverse").append("\r\n");
+        builder.append("    ").append(GetCsharpColorProperty("InverseSurface", scheme.getInverseSurface())).append("\r\n");
+        builder.append("    ").append(GetCsharpColorProperty("InverseOnSurface", scheme.getInverseOnSurface())).append("\r\n");
+        builder.append("    ").append(GetCsharpColorProperty("InversePrimary", scheme.getInversePrimary())).append("\r\n");
+
+        // Primary Fixed
+        builder.append("    ").append("// Primary Fixed").append("\r\n");
+        builder.append("    ").append(GetCsharpColorProperty("PrimaryFixed", scheme.getPrimaryFixed())).append("\r\n");
+        builder.append("    ").append(GetCsharpColorProperty("OnPrimaryFixed", scheme.getOnPrimaryFixed())).append("\r\n");
+        builder.append("    ").append(GetCsharpColorProperty("PrimaryFixedDim", scheme.getPrimaryFixedDim())).append("\r\n");
+        builder.append("    ").append(GetCsharpColorProperty("OnPrimaryFixedVariant", scheme.getOnPrimaryFixedVariant())).append("\r\n");
+
+        // Secondary Fixed
+        builder.append("    ").append("// Secondary Fixed").append("\r\n");
+        builder.append("    ").append(GetCsharpColorProperty("SecondaryFixed", scheme.getSecondaryFixed())).append("\r\n");
+        builder.append("    ").append(GetCsharpColorProperty("OnSecondaryFixed", scheme.getOnSecondaryFixed())).append("\r\n");
+        builder.append("    ").append(GetCsharpColorProperty("SecondaryFixedDim", scheme.getSecondaryFixedDim())).append("\r\n");
+        builder.append("    ").append(GetCsharpColorProperty("OnSecondaryFixedVariant", scheme.getOnSecondaryFixedVariant())).append("\r\n");
+
+        // Tertiary Fixed
+        builder.append("    ").append("// Tertiary Fixed").append("\r\n");
+        builder.append("    ").append(GetCsharpColorProperty("TertiaryFixed", scheme.getTertiaryFixed())).append("\r\n");
+        builder.append("    ").append(GetCsharpColorProperty("OnTertiaryFixed", scheme.getOnTertiaryFixed())).append("\r\n");
+        builder.append("    ").append(GetCsharpColorProperty("TertiaryFixedDim", scheme.getTertiaryFixedDim())).append("\r\n");
+        builder.append("    ").append(GetCsharpColorProperty("OnTertiaryFixedVariant", scheme.getOnTertiaryFixedVariant())).append("\r\n");
+
+        // Surface Container
+        builder.append("    ").append("// Surface Container").append("\r\n");
         builder.append("    ").append(GetCsharpColorProperty("SurfaceContainerLowest", scheme.getSurfaceContainerLowest())).append("\r\n");
         builder.append("    ").append(GetCsharpColorProperty("SurfaceContainerLow", scheme.getSurfaceContainerLow())).append("\r\n");
         builder.append("    ").append(GetCsharpColorProperty("SurfaceContainer", scheme.getSurfaceContainer())).append("\r\n");
         builder.append("    ").append(GetCsharpColorProperty("SurfaceContainerHigh", scheme.getSurfaceContainerHigh())).append("\r\n");
         builder.append("    ").append(GetCsharpColorProperty("SurfaceContainerHighest", scheme.getSurfaceContainerHighest())).append("\r\n");
+
         builder.append("}").append("\r\n");
 
         var fileContent = builder.toString();
@@ -82,11 +144,11 @@ public class CsharpTestThemeCreator {
     }
 
     private static DynamicScheme CreateScheme(Hct hct,
-                                       boolean isDark,
-                                       double contrastLevel,
-                                       ColorSpec.SpecVersion specVersion,
-                                       DynamicScheme.Platform platform,
-                                       String variantName){
+                                              boolean isDark,
+                                              double contrastLevel,
+                                              ColorSpec.SpecVersion specVersion,
+                                              DynamicScheme.Platform platform,
+                                              String variantName){
         return switch (variantName) {
             case "Monochrome" -> new SchemeMonochrome(hct, isDark, contrastLevel, specVersion, platform);
             case "Neutral" -> new SchemeNeutral(hct, isDark, contrastLevel, specVersion, platform);
@@ -103,28 +165,28 @@ public class CsharpTestThemeCreator {
 
     private static Hct GetColor(int index){
         Hct[] colors = new Hct[] {
-            Hct.from(25, 70, 50),
-            Hct.from(50, 50, 60),
-            Hct.from(110, 50, 70),
-            Hct.from(150, 50, 50),
-            Hct.from(210, 60, 70),
-            Hct.from(260, 50, 40),
-            Hct.from(310, 60, 50),
-            Hct.from(350, 70, 60)
+                Hct.from(25, 70, 50),
+                Hct.from(50, 50, 60),
+                Hct.from(110, 50, 70),
+                Hct.from(150, 50, 50),
+                Hct.from(210, 60, 70),
+                Hct.from(260, 50, 40),
+                Hct.from(310, 60, 50),
+                Hct.from(350, 70, 60)
         };
         var sanitizedIndex = ((index % colors.length) + colors.length) % colors.length;
         return colors[sanitizedIndex];
     }
     private static String GetColorName(int index){
         String[] colors = new String[] {
-            "Red",
-            "Orange",
-            "Yellow",
-            "Green",
-            "Cyan",
-            "Blue",
-            "Purple",
-            "Magenta"
+                "Red",
+                "Orange",
+                "Yellow",
+                "Green",
+                "Cyan",
+                "Blue",
+                "Purple",
+                "Magenta"
         };
         var sanitizedIndex = ((index % colors.length) + colors.length) % colors.length;
         return colors[sanitizedIndex];
@@ -155,9 +217,12 @@ public class CsharpTestThemeCreator {
         return "Spec2021";
     }
 
-
     private static String GetColorFromArgb(int argb){
         var hexString = Integer.toHexString(argb);
+        // Ensure hex string has leading zeros if necessary
+        while(hexString.length() < 8) {
+            hexString = "0" + hexString;
+        }
         var withoutAlpha = hexString.substring(2);
         return "#" + withoutAlpha;
     }
